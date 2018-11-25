293493390e7bedcca71e1f950072d245d929b54a
Atul Kumar Sinha <atulkumarin@gmail.com>
2018-11-25 17:31:15 +0100

    preliminary data analysis - WIP

---
+++ Datasets/download.sh
@@ -0,0 +1,4 @@
+wget http://resources.mpi-inf.mpg.de/impact/dl_cred_analysis/Snopes.zip
+unzip Snopes.zip
+wget http://resources.mpi-inf.mpg.de/impact/dl_cred_analysis/PolitiFact.zip
+unzip PolitiFact.zip
---
+++ Glove/download.sh
@@ -0,0 +1,2 @@
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip
